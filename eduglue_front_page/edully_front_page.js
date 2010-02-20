1b8a5131c8370f877535905e30093ed662988835
Kyle Mathews <mathews.kyle@gmail.com>
2010-02-20 15:49:40 -0500

    Removed eduglu_group_block as is unnecessary with new theme.

--- eduglue_front_page/edully_front_page.js
+++ eduglue_front_page/edully_front_page.js
@@ -1,30 +1,14 @@
 // Global killswitch: only run if we are in a supported browser.
 if (Drupal.jsEnabled) {
-  // Set event handler. Listen for clicks and toggle between minimized and
-  // maximized versions of discussions.
-  $(document).ready(
-    function(){
-      $("#show-more-plus").click(function(){
-        if ($('#popular-conversations ol').children().slice(19,20).css('display') == 'none') {
-          // Remove styles to show. Can't use toggle or show as numbers in
-          // ordered lists don't show if there's a "display: block" in the inline css.
-          $('#popular-conversations ol').children().slice(10,20).removeAttr('style');
-          $('#show-more-plus').html("-");
-          $('#show-more-help-text').text('Hide');
-          
-        }
-        else {
-          $('#popular-conversations ol').children().slice(10,20).hide();
-          $('#show-more-plus').html("+");
-          $('#show-more-help-text').text('Show more popular conversations');
-          
-        }
-        
-        return false;
+  Drupal.behaviors.eduglu_front_page = function() {
+    $(document).ready( function() {
+      $(".activity-teaser-expand").click(function() {
+          $(this).parent().hide();
+          $(this).parent().next().next().addClass("prefix-2 alpha");
+          $(this).parent().next().show();
+      
+          return false;
       });
-    }
-  );
-
-$('#popular-conversations ol').children().slice(10,20).hide();
-
+    });
+  }
 }
